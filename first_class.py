c5c252457aacbc13759bb99426572b24135a0dc1
shavendra <shavendragoesoft@gmail.com>
2022-01-28 11:22:01 +0530

    add method with instance acess

--- first_class.py
+++ first_class.py
@@ -3,9 +3,20 @@
 
 
 class Employee:  # this thing is a blue print of the class 
-   def __init__(self , first_name , secound_name ) -> None:  # constructor 
+    def __init__(self , first_name , secound_name ) -> None:  # constructor 
        self.first_name = first_name
        self.secound_name = secound_name
+    
+    def fullName(self): # based on the explaination it's mention they need first instance for access to the internal parametes 
+        print(self.first_name)  # not like static lagugage " kalin waliw eketa access wenna oyaya self instance eke parameter ekaka widihata send karanna one "
+        return f"{self.first_name} {self.secound_name}"
+        
+    
+    
+
+
+
+    
 
 emp_1 = Employee("shavendra","Ekanayake")
 emp_2 = Employee("kanishke" ,"Ekanayake") 
@@ -13,7 +24,9 @@ emp_2 = Employee("kanishke" ,"Ekanayake")
 print(emp_1.first_name)
 print(emp_2.first_name)
 
-emp_2.age = 27  # this is why python is not good oop language 
+
+print(emp_1.fullName())
+
 
 
 

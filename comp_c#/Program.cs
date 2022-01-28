f70a3914d336a653f97e11684bfada1e2aa5b10a
shavendra <shavendragoesoft@gmail.com>
2022-01-28 12:32:56 +0530

    ignore

---
+++ comp_c#/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using shavendra;
+
+namespace  shavendra
+{
+
+    public class SimpleTest{
+
+        public SimpleTest()
+        {
+            this.simplemethod();
+        }
+
+        private void simplemethod()
+        {            
+            Console.WriteLine("working"); 
+        }
+    }
+
+   
+}
+
+
+
+class Program{
+    public static void Main(string[] args){
+        var s = new SimpleTest();
+      
+        // Console.WriteLine(s.First_name);
+    }
+
+}

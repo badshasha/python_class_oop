d3fb484033ced4106a7671eedaa042403525c44f
shavendra <shavendragoesoft@gmail.com>
2022-01-28 10:49:22 +0530

    instance and object

---
+++ first_class.py
@@ -0,0 +1,18 @@
+# code by 'badshasha'
+# purpose : learning python oop concept 
+
+
+class Employee:  # this thing is a blue print of the class 
+    pass
+
+emp_1 = Employee() # this are the instances of the project 
+emp_2 = Employee() # this is a another instance 
+
+
+print(emp_1)
+print(emp_2)
+
+# <__main__.Employee object at 0x7f9d08adbcf8>
+# <__main__.Employee object at 0x7f9d08adbef0>   each instance have got seperate memeory location 
+
+
